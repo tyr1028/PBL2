13cbc58d931fbdcc6d1f6d2904d81ebf6c2ad2c0
Leejooyeol <gametyr@naver.com>
2020-12-09 15:11:15 +0900

    update mypanel

--- MyPanel.java
+++ MyPanel.java
@@ -1,33 +1,52 @@
-
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
 /**
  * Write a description of class MyPanel here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class MyPanel
+public class MyPanel extends JPanel implements ActionListener
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class MyPanel
-     */
-    public MyPanel()
-    {
-        // initialise instance variables
-        x = 0;
+    JTextField ntf;
+    JTextField dptf;
+    JTextField adtf;
+    JButton add;
+    JButton clear;
+    JComboBox gradeBox;
+    JScrollPane textArea;
+    public MyPanel(){
+        JLabel nlabel = new JLabel("이름");
+        JLabel dplabel = new JLabel("학과");
+        JLabel adlabel = new JLabel("주소");
+        JLabel gradelabel = new JLabel("학년");
+        
+        ntf = new JTextField("홍길동",20);
+        dptf = new JTextField("글로벌소프트웨어학과",20);
+        adtf = new JTextField("아산시...",20);
+        
+        add = new JButton("추가");
+        clear = new JButton("Clear");
+        
+        gradeBox = new JComboBox();
+        
+        textArea = new JScrollPane(new JTextArea(8 , 22));
+        
+        this.add(nlabel);
+        this.add(ntf);
+        this.add(dplabel);
+        this.add(dptf);
+        this.add(adlabel);
+        this.add(adtf);
+        this.add(gradelabel);
+        this.add(gradeBox);
+        this.add(add);
+        this.add(clear);
+        this.add(textArea);
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public void actionPerformed(ActionEvent e){
+    
     }
 }
