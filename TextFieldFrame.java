1821703d81eaab7194db4b3bcde5def5e8ab8453
Kim Minsoo <k21332133@naver.com>
2020-12-09 14:52:40 +0900

    makeTextFieldFrame

--- TextFieldFrame.java
+++ TextFieldFrame.java
@@ -1,33 +1,20 @@
-
+import javax.swing.*;
 /**
- * Write a description of class TextFieldFrame here.
+ * Frame
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (2019315007 이주열, 2019315015 김민수)
+ * @version (2020.12.09)
  */
-public class TextFieldFrame
+public class TextFieldFrame extends JFrame
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class TextFieldFrame
-     */
-    public TextFieldFrame()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public TextFieldFrame() {
+        this.setTitle("과제#3(2019315007 이주열, 2019315015 김민수)");
+        this.setSize(300, 1000);
+        
+        MyPanel mp = new MyPanel();
+        this.add(mp);
+        
+        this.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
